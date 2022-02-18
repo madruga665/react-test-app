0a11b532dd5f3fe9d07e92015b7dcb96be8d3b7b
Carlos Martorini <carlos.silveira@remessaonline.com.br>
2022-02-18 12:27:05 -0300

    yup sem componente

--- src/Pages/FormsPage/index.jsx
+++ src/Pages/FormsPage/index.jsx
@@ -1,36 +1,43 @@
 import React from "react";
-import Form from "../../components/Form";
-import InputField from "../../components/InputField";
 import styles from "./styles.module.scss";
-// import { yupResolver } from '@hookform/resolvers/yup';
-// import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { useForm } from "react-hook-form";
 
-// const schema = yup.object({
-//   name: yup.string().required('Coloca o nome ai mano'),
-//   email: yup.string().email().required('Deu ruim parsa'),
-// }).required();
 
 const FormsPage = () => {
-  const { register, handleSubmit, reset, formState:{ errors }  } = useForm({
-    // resolver: yupResolver(schema)
+  const schema = yup.object().shape({
+     name: yup.string().required('Coloca o nome ai mano'),
+     email: yup.string().email().required('Deu ruim parsa'),
+     phone: yup.number().required(),
+     address: yup.string().required()
   });
+
+  const { register, handleSubmit, reset, formState:{errors} } = useForm ({
+    resolver: yupResolver(schema) 
+  })
+
   const onSubmit = (data) => {
     console.log(data);
-    console.log(errors)
     reset()
   }
+
   return (
     <div className={styles.FormsPageContainer}>
       <h2>Formulário</h2>
-      <Form handleSubmit={handleSubmit} onSubmit={onSubmit}>
-        <InputField id='name' label='Name' type='text' register={register} />
-        <p>{errors.name?.message}</p>
-        <InputField id='email' label='Email' type='email' register={register} />
-        <InputField id='phone' label='Phone' type='text' register={register} />
-        <InputField id='address' label='Address' type='text' register={register} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('name')} />
+        <p className='error'>
+          {errors.name?.message}
+        </p>
+        <input {...register('email')} />
+        <p className='error'>
+          {errors.email?.message}
+        </p>
+        <input {...register('phone')} />
+        <input {...register('address')} />
         <button type='submit'>Enviar a Bagaça</button>
-      </Form>
+      </form>
     </div>
   );
 };
