168a774a2cd54bd329b9319aa08d13b24de4e54c
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-02-20 10:59:50 -0300

    add variaveis de cor

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,16 +1,17 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { Colors } from "../../styles/variables";
 
 export const ButtonStyled = styled.button`
-  color: palevioletred;
+  color: ${Colors.primaryBlue};
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+  border: 2px solid ${Colors.primaryBlue};
   border-radius: 3px;
   transition: 500ms;
   &:hover {
     color: white;
-    background-color: palevioletred;
+    background-color: ${Colors.primaryBlue};
     cursor: pointer;
   }
 `;
---
+++ src/styles/variables.js
@@ -0,0 +1,3 @@
+export const Colors = {
+  primaryBlue: '#3498db',
+}
