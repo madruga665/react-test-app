3639e22dcfa83406bcc9f46f29c1b3f3f2303673
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-02-23 10:47:57 -0300

    add typescript no component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import { HeaderContainer, NavContainer } from './styles'
 
-const Header = ({title, link1, link2, linkTitle1, linkTitle2}) => {
+interface IHeaderProps {
+  title: string;
+  link1: string;
+  link2: string;
+  linkTitle1: string;
+  linkTitle2: string;
+}
+
+const Header = ({title, link1, link2, linkTitle1, linkTitle2}: IHeaderProps) => {
   return (
     <HeaderContainer>
       <h1>{title}</h1>
